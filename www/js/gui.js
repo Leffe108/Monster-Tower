f0a315cc173d18bf2eb5cc1c9956b7c1f8cafb63
Leif Linse <leif.linse@gmail.com>
2015-12-30 13:37:38 +0100

    -Fix (64c5ba3): Silence ESLint error

--- www/js/gui.js
+++ www/js/gui.js
@@ -5,7 +5,7 @@
 /* global Building, GameLevel, Money, Room, RoomType, SaveLoad, MtImage, BuildNewCursor */
 /* global g_bank_balance:true, g_canvas, g_dirty_screen:true, g_logo_timer:true, g_game_star_level, g_room_types, g_room_floors, g_stair_floors, g_view_offset_x:true, g_view_offset_floor:true, g_intro_aria_set:true */
 /* global EncodeEntities, InitGameState, MapToScreen, MoneyStr, StrFirstToUpper, DISABLE_LOGO_INTRO:true */
-/* exported g_dirty_screen, g_logo_timer, DISABLE_LOGO_INTRO */
+/* exported g_dirty_screen, g_logo_timer, g_intro_aria_set, DISABLE_LOGO_INTRO */
 
 /* exported Gui */
 var Gui = (function() {
